2f7d640eb72ab26ad30fbd451ad68d6d560dac1d
liliana paola <lilianapaolaesmeral@gmail.com>
2022-06-05 13:45:26 -0500

    add animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Personajes from './components/personajes';
 
 function App() {
   return (
-    <div>
+    <div className='animate__animated animate__fadeIn animate__delay-2s'>
       <img 
         src={'https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVDCqfY6MmG6BJjO9v_vTUyJEW_KmdUSuXhTKq8iZHHibxHWJDNwkyzKh8SI3scwt57yuA0gyWtpk6Yb1WT7Jjl0Tj4HoszEEpxri5NNDRbr.png?r=d7f'} 
         className='banner'
