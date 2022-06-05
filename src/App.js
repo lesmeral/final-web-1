db7f092865d75e28542ab0321a115151f49ced6a
liliana paola <lilianapaolaesmeral@gmail.com>
2022-06-05 13:18:24 -0500

    add componente para agregar personajes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Personajes from './components/personajes';
 
 function App() {
   return (
     <div>
-      <img src='https://static.wikia.nocookie.net/wiki-de-rick-morty/images/6/60/Rick_and_Morty.png/revision/latest?cb=20210623034924&path-prefix=es' alt='title'/>
-      
+      <img 
+        src={'https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVDCqfY6MmG6BJjO9v_vTUyJEW_KmdUSuXhTKq8iZHHibxHWJDNwkyzKh8SI3scwt57yuA0gyWtpk6Yb1WT7Jjl0Tj4HoszEEpxri5NNDRbr.png?r=d7f'} 
+        className='banner'
+      />
+      <Personajes />
     </div>
   );
 }
---
+++ src/components/personajes.jsx
@@ -0,0 +1,11 @@
+
+
+const Personajes = () => {
+  return(
+    <div>
+      personajes
+    </div>
+  )
+}
+
+export default Personajes
